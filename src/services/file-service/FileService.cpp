0aa5b99ae94de747bcc90ff6d8891218c0f88624
Volkov Daniil <volkov.study@mail.ru>
2023-01-19 20:18:21 +0300

    Implement FileService#readAll()

--- src/services/file-service/FileService.cpp
+++ src/services/file-service/FileService.cpp
@@ -2,6 +2,21 @@
 
 FileService::FileService(const string &filePath) : _filePath(filePath) {}
 
+vector<string> FileService::readAll() {
+    ifstream input(_filePath);
+
+    vector<string> result;
+
+    string line;
+    while (getline(input, line)) {
+        if (line.empty()) break;
+
+        result.push_back(line);
+    }
+
+    return result;
+}
+
 void FileService::write(const string &data) {
     ofstream output(_filePath, ios::app);
 

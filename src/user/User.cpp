bfaf1dec7d0bbec3afbc1ab005fbef710a52036b
Volkov Daniil <volkov.study@mail.ru>
2023-01-19 19:35:58 +0300

    Add main fields & getters in User class

--- src/user/User.cpp
+++ src/user/User.cpp
@@ -1,1 +1,9 @@
 #include "User.h"
+
+int User::getId() const {
+    return _id;
+}
+
+const string &User::getName() const {
+    return _name;
+}
--- src/user/User.h
+++ src/user/User.h
@@ -2,7 +2,18 @@
 #define VIDEO_HOSTING_USER_H
 
 
+#include <string>
+
+using namespace std;
+
 class User {
+private:
+    int _id;
+    string _name;
+    string _password;
+public:
+    int getId() const;
+    const string &getName() const;
 };
 
 

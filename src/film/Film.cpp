d6cd9fd1c0a3b15a6948c0ab19778e4ed327eb95
Volkov Daniil <volkov.study@mail.ru>
2023-01-19 19:30:46 +0300

    Add main fields & getters in Film class

--- src/film/Film.cpp
+++ src/film/Film.cpp
@@ -1,1 +1,9 @@
 #include "Film.h"
+
+const string &Film::getTitle() const {
+    return _title;
+}
+
+int Film::getMark() const {
+    return _mark;
+}
--- src/film/Film.h
+++ src/film/Film.h
@@ -2,7 +2,17 @@
 #define VIDEO_HOSTING_FILM_H
 
 
+#include <string>
+
+using namespace std;
+
 class Film {
+public:
+    const string &getTitle() const;
+    int getMark() const;
+private:
+    string _title;
+    int _mark;
 };
 
 

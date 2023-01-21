bafe12f9f3676d96c8a658aeda5d66c8da0352cd
Volkov Daniil <volkov.study@mail.ru>
2023-01-21 19:44:29 +0300

    Delete tests from src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,5 @@
 #include <iostream>
-#include "services/file-service/FileService.h"
 
 int main() {
-    FileService fileService("test.txt");
-
-    fileService.write("test data");
-    fileService.write("another line of data");
-
-    vector<string> lines = fileService.readAll();
-
-    for (const string& line: lines) {
-        cout << line << endl;
-    }
-
     return 0;
 }

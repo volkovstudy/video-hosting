30ea223444e5307c2f0eedfa126d76e813b4bb93
Volkov Daniil <volkov.study@mail.ru>
2023-01-22 17:34:43 +0300

    Add new method FilmsLineParser::parse(string...)

--- src/repositories/films-repository/films-line-parser/FilmsLineParser.cpp
+++ src/repositories/films-repository/films-line-parser/FilmsLineParser.cpp
@@ -1,1 +1,43 @@
 #include "FilmsLineParser.h"
+
+#define databaseDelimiter ','
+
+vector<string> split(const string& line, char delimiter);
+map<string, string> convertLineToFields(const string& line);
+
+Film FilmsLineParser::parse(const string& line) {
+    map<string, string> fields = convertLineToFields(line);
+
+    Film film(
+            stoi(fields["id"]),
+            fields["title"],
+            stoi(fields["mark"])
+    );
+
+    return film;
+}
+
+vector<string> split(const string& line, char delimiter) {
+    vector<string> result;
+
+    stringstream stream(line);
+    string item;
+
+    while (getline(stream, item, delimiter)) {
+        result.push_back(item);
+    }
+
+    return result;
+}
+
+map<string, string> convertLineToFields(const string& line) {
+    map<string, string> fields;
+    vector<string> splitLine = split(line, databaseDelimiter);
+
+    // Line format: id,title,mark
+    fields["id"] = splitLine.at(0);
+    fields["title"] = splitLine.at(1);
+    fields["mark"] = splitLine.at(2);
+
+    return fields;
+}
--- src/repositories/films-repository/films-line-parser/FilmsLineParser.h
+++ src/repositories/films-repository/films-line-parser/FilmsLineParser.h
@@ -2,7 +2,16 @@
 #define VIDEO_HOSTING_FILMSLINEPARSER_H
 
 
+#include "models/film/Film.h"
+
+#include <string>
+#include <sstream>
+#include <map>
+#include <vector>
+
 class FilmsLineParser {
+public:
+    static Film parse(const string& line);
 };
 
 

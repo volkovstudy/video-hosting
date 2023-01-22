51cd681fc60c747a755e0270eb68131db6f8d8c0
Volkov Daniil <volkov.study@mail.ru>
2023-01-22 17:45:33 +0300

    Add new method: Utils::split(...)

--- src/services/utils/Utils.cpp
+++ src/services/utils/Utils.cpp
@@ -1,1 +1,14 @@
 #include "Utils.h"
+
+vector<string> Utils::split(const string& line, char delimiter) {
+    vector<string> result;
+
+    stringstream stream(line);
+    string item;
+
+    while (getline(stream, item, delimiter)) {
+        result.push_back(item);
+    }
+
+    return result;
+}
--- src/services/utils/Utils.h
+++ src/services/utils/Utils.h
@@ -2,7 +2,15 @@
 #define VIDEO_HOSTING_UTILS_H
 
 
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
 class Utils {
+public:
+    static vector<string> split(const string& line, char delimiter);
 };
 
 

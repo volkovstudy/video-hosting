cf980ccee11d6b1134c0615aa0230ca6b2c1a75c
Volkov Daniil <volkov.study@mail.ru>
2023-01-22 18:16:36 +0300

    Create test for FilmsRepository#findAll()

---
+++ tests/films-repository-tests/FilmsRepositoryTests.cpp
@@ -0,0 +1,43 @@
+#include <cassert>
+
+#include "repositories/films-repository/FilmsRepository.h"
+
+#define testFile "films-repository-test.txt"
+#define databaseDelimiter ','
+
+void shouldFindAllFilms();
+
+int main() {
+    shouldFindAllFilms();
+    ::remove(testFile);
+
+    return 0;
+}
+
+vector<Film> givenTwoFilmsInDatabase() {
+    vector<Film> films = {
+            Film(1, "The Fast and The Furious", 99),
+            Film(2, "Cheburashka", 50)
+    };
+
+    FileService fileService(testFile);
+
+    for (const Film& film: films) {
+        stringstream line;
+        line << film.getId() << databaseDelimiter << film.getTitle() << databaseDelimiter << film.getMark();
+
+        fileService.write(line.str());
+    }
+
+    return films;
+}
+
+void shouldFindAllFilms() {
+    vector<Film> givenFilms = givenTwoFilmsInDatabase();
+
+    FilmsRepository filmsRepository(testFile);
+
+    vector<Film> foundFilms = filmsRepository.findAll();
+
+    assert(givenFilms == foundFilms);
+}
